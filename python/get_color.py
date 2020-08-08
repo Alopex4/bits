06d5141ae111300c6b8fecf128cbf4822c7f0cad
leslie <leslie@163.com>
2020-08-08 19:10:53 +0800

    add color

---
+++ python/get_color.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# [src](https://github.com/reorx/python-terminal-color/blob/master/color_simple.py)
+r""" 
+Simple color code snip.
+
+[usage]
+    red('hello world')
+"""
+
+import sys
+
+# check is tty like `fd` or not
+ISATTY = sys.stdout.isatty()
+
+
+def make_color(code):
+    def color_func(s):
+        if not ISATTY:
+            return s
+        tpl = '\x1b[{}m{}\x1b[0m'
+        return tpl.format(code, s)
+    return color_func
+
+
+# format
+bold = make_color(1)
+underline = make_color(4)
+
+# color code
+black = make_color(30)
+red = make_color(31)
+green = make_color(32)
+yellow = make_color(33)
+blue = make_color(34)
+magenta = make_color(35)
+cyan = make_color(36)
+white = make_color(37)
